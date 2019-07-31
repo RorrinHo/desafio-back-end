924d0681f90c9d39e375b14db12733e49d74c714
Rodrigo Saldivia <rolo.sr@gmail.com>
2019-07-31 16:08:55 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
+
 
 const indexRouter = require('./routes/index');
 const catalogoRouter = require("./routes/catalogo");
@@ -14,7 +14,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
